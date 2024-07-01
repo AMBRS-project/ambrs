bedd3c7b72a0224280eaa31bb76efbc383e82343
Jeffrey N. Johnson <jeff@cohere-llc.com>
2024-07-01 13:58:40 -0700

    First cut at latin hypercube sampling.

--- ambrs/aerosol.py
+++ ambrs/aerosol.py
@@ -1,14 +1,19 @@
 """aerosol - aerosol species and particle size distribution machinery
 
-We use scipy.stats distributions (specifically the rv_continuous ones) for
-sampling. Because scipy provides an extensible framework for sampling, any
-type of sampling process (including sampling from ESMs like E3SM) is possible.
+We use frozen scipy.stats distributions (specifically the rv_continuous ones)
+for sampling. These frozen distributions fix the shape parameters, location, and
+scale factors for each random variable.
 """
 
 import numpy as np
 import scipy.stats
 
 from dataclasses import dataclass
+from typing import TypeVar
+
+# this type represents a frozen scipy.stats.rv_continous distribution
+# (this frozen type isn't made available by the scipy.stats package)
+RVFrozenDistribution = TypeVar('RVFrozenDistribution')
 
 @dataclass
 class AerosolProcesses:
@@ -19,22 +24,22 @@ class AerosolProcesses:
     mosaic: bool
     nucleation: bool
 
-@dataclass
+@dataclass(frozen=True)
 class AerosolSpecies:
     """AerosolSpecies: the definition of an aerosol species in terms of species-
 specific parameters (no state information)"""
     name: str          # name of the species
     molar_mass: float  # etc
 
-@dataclass
+@dataclass(frozen=True)
 class AerosolModeDistribution:
     """AerosolModeDistribution: the definition of a single internally-mixed,
 log-normal aerosol mode (configuration only--no state information)"""
     name: str
-    species: tuple[AerosolSpecies]
-    number: scipy.stats.rv_continuous                # modal number concentration distribution
-    geom_mean_diam: scipy.stats.rv_continuous        # geometric mean diameter distribution
-    mass_fractions: tuple[scipy.stats.rv_continuous] # species mass fraction distributions
+    species: tuple[AerosolSpecies, ...]
+    number: RVFrozenDistribution                     # modal number concentration distribution
+    geom_mean_diam: RVFrozenDistribution             # geometric mean diameter distribution
+    mass_fractions: tuple[RVFrozenDistribution, ...] # species mass fraction distributions
 
 @dataclass
 class AerosolModePopulation:
@@ -43,16 +48,16 @@ internally-mixed, log-normal aerosol mode, sampled from a specific mode
 distribution"""
     number: np.array
     geom_mean_diam: np.array
-    mass_fractions: np.array
+    mass_fractions: tuple[np.array, ...] # species-specific mass fractions
 
 @dataclass
 class AerosolModalSizeDistribution:
     """AerosolModalSizeDistribution: an aerosol particle size distribution
 specified in terms of a fixed number of log-normal modes"""
-    modes: tuple[AerosolModeDistribution]
+    modes: tuple[AerosolModeDistribution, ...]
     
 @dataclass
 class AerosolModalSizePopulation:
     """AerosolModalSizePopulation: an aerosol population sampled from a specific
 modal particle size distribution"""
-    modes: tuple[AerosolModePopulation]
+    modes: tuple[AerosolModePopulation, ...]
--- ambrs/ppe.py
+++ ambrs/ppe.py
@@ -1,55 +1,93 @@
 """ambrs.ppe - types and functions supporting the creation of perturbed-parameter
 ensembles (PPE).
 
-We use scipy.stats distributions (specifically the rv_continuous ones) for
-sampling. Because scipy provides an extensible framework for sampling, any
-type of sampling process (including sampling from ESMs like E3SM) is possible.
+We use frozen scipy.stats distributions (specifically the rv_continuous ones)
+for sampling. These frozen distributions fix the shape parameters, location, and
+scale factors for each random variable.
 """
 
 import numpy as np
+import pyDOE
 import scipy.stats
 
 from dataclasses import dataclass
-from .aerosol import AerosolModalSizeDistribution, AerosolModalSizePopulation
+from .aerosol import AerosolModalSizeDistribution, AerosolModalSizePopulation,\
+                     RVFrozenDistribution
 
-@dataclass
+@dataclass(frozen=True)
 class EnsembleSpecification:
     """EnsembleSpecification: a set of distributions from which members of a
 PPE are sampled"""
     name: str
-    modalSize: AerosolModalSizeDistribution
-    flux: scipy.stats.rv_continuous
-    relativeHumidity: scipy.stats.rv_continuous
-    temperature: scipy.stats.rv_continuous
+    modal_size: AerosolModalSizeDistribution
+    flux: RVFrozenDistribution
+    relative_humidity: RVFrozenDistribution
+    temperature: RVFrozenDistribution
 
-@dataclass
+@dataclass(frozen=True)
 class Ensemble:
     """Ensemble: an ensemble defined by values sampled from the distributions of
 a specific EnsembleSpecification"""
     specification: EnsembleSpecification  # used to construct Ensemble
-    modalSize: AerosolModalSizePopulation
+    modal_size: AerosolModalSizePopulation
     flux: np.array
-    relativeHumidity: np.array
+    relative_humidity: np.array
     temperature: np.array
 
-# TODO: demonstrate how to incorporate experiment design like latin hypercube
 def sample(spec: EnsembleSpecification,
            n:    int) -> Ensemble:
     """sample(spec, n) -> n-member ensemble sampled from spec"""
-    modalSize = None
-    if spec.modalSize:
-        modalSize = AerosolModalSizePopulation(
+    modal_size = None
+    if spec.modal_size:
+        modal_size = AerosolModalSizePopulation(
             modes=tuple([
                 AerosolModePopulation(
-                    number=spec.modalSize.number.rvs(n),
-                    geom_mean_diam=spec.modalSize.geom_mean_diam.rvs(n),
-                    mass_fractions=tuple([f.rvs(n) for f in spec.modalSize.mass_fractions]),
-                ) for m in distribution.modes]),
+                    number=mode.number.rvs(n),
+                    geom_mean_diam=mode.geom_mean_diam.rvs(n),
+                    mass_fractions=tuple([f.rvs(n) for f in mode.mass_fractions]),
+                ) for mode in spec.modal_size.modes]),
         )
     return Ensemble(
         specification = spec,
-        modalSize = modalSize,
+        modal_size = modal_size,
         flux = spec.flux.rvs(n),
-        relativeHumidity = spec.relativeHumidity.rvs(n),
+        relative_humidity = spec.relative_humidity.rvs(n),
         temperature = spec.temperature.rvs(n),
+    )
+
+def lhs(spec: EnsembleSpecification,
+        n: int,
+        criterion = None,
+        iterations = None) -> Ensemble:
+    """lhs(spec, n, [criterion, iterations]) -> n-member ensemble generated
+from latin hypercube sampling applied to the given specification. The optional
+arguments are passed along to pyDOE's lhs function, which creates the
+distribution from which ensemble members are sampled."""
+    n_factors = 3 # size-independent factors: flux + relative_humidity + temperature
+    lhd = None # latin hypercube distribution (created depending on particle
+               # size representation)
+    modal_size = None
+    if spec.modal_size:
+        # count up mode-related factors
+        for mode in spec.modal_size.modes:
+            n_factors += 2 + len(mode.mass_fractions)
+        # lhd is a 2D array with indices (sample index, factor index)
+        lhd = pyDOE.lhs(n_factors, n, criterion, iterations)
+        num_species = len(spec.modal_size.species)
+        modal_size = AerosolModalSizePopulation(
+            modes=tuple([
+                AerosolModePopulation(
+                    number=mode.number.pdf(lhd[:,(2+num_species)*m]),
+                    geom_mean_diam=mode.geom_mean_diam.pdf(lhd[:,(2+num_species+1)*m]),
+                    mass_fractions=tuple(
+                        [mass_fraction.pdf(lhd[:,(2*num_species+2)*m+f])
+                         for f,mass_fraction in enumerate(mode.mass_fractions)]),
+                ) for m, mode in enumerate(spec.modal_size.modes)]),
+        )
+    return Ensemble(
+        specification = spec,
+        modal_size = modal_size,
+        flux = spec.flux.rvs(n),
+        relative_humidity = spec.relative_humidity.pdf(lhd[:,-2]),
+        temperature = spec.temperature.pdf(lhd[:,-1]),
     )
