e2a938dabd745272dc04af1aa358e8047266b99a
Jeffrey N. Johnson <jeff@cohere-llc.com>
2024-09-18 12:59:42 -0700

    Fixed unit tests.

--- test/test_ppe.py
+++ test/test_ppe.py
@@ -10,16 +10,55 @@ import scipy.stats
 import unittest
 
 # relevant aerosol and gas species
-so4 = aerosol.AerosolSpecies(name='so4', molar_mass = 97.071)
-pom = aerosol.AerosolSpecies(name='pom', molar_mass = 12.01)
-soa = aerosol.AerosolSpecies(name='soa', molar_mass = 12.01)
-bc  = aerosol.AerosolSpecies(name='bc', molar_mass = 12.01)
-dst = aerosol.AerosolSpecies(name='dst', molar_mass = 135.065)
-ncl = aerosol.AerosolSpecies(name='ncl', molar_mass = 58.44)
+so4 = aerosol.AerosolSpecies(
+    name='so4',
+    molar_mass = 97.071, # NOTE: 1000x smaller than "molecular weight"!
+    density = 1770,
+    hygroscopicity = 0.507,
+)
+pom = aerosol.AerosolSpecies(
+    name='pom',
+    molar_mass = 12.01,
+    density = 1000,
+    hygroscopicity = 0.5,
+)
+soa = aerosol.AerosolSpecies(
+    name='soa',
+    molar_mass = 12.01,
+    density = 1000,
+    hygroscopicity = 0.5,
+)
+bc = aerosol.AerosolSpecies(
+    name='bc',
+    molar_mass = 12.01,
+    density = 1000,
+    hygroscopicity = 0.5,
+)
+dst = aerosol.AerosolSpecies(
+    name='dst',
+    molar_mass = 135.065,
+    density = 1000,
+    hygroscopicity = 0.5,
+)
+ncl = aerosol.AerosolSpecies(
+    name='ncl',
+    molar_mass = 58.44,
+    density = 1000,
+    hygroscopicity = 0.5,
+)
 
-so2   = gas.GasSpecies(name='so2', molar_mass = 64.07)
-h2so4 = gas.GasSpecies(name='h2so4', molar_mass = 98.079)
-soag  = gas.GasSpecies(name='soag', molar_mass = 12.01)
+so2 = gas.GasSpecies(
+    name='so2',
+    molar_mass = 64.07,
+)
+h2so4 = gas.GasSpecies(
+    name='h2so4',
+    molar_mass = 98.079,
+)
+soag = gas.GasSpecies(
+    name='soag',
+    molar_mass = 12.01,
+)
 
 # reference pressure and height
 p0 = 101325 # [Pa]
@@ -40,6 +79,7 @@ class TestEnsemble(unittest.TestCase):
                         species = [so4, soa, ncl],
                         number = 5e8,
                         geom_mean_diam = 1e-7,
+                        log10_geom_std_dev = 1.6,
                         mass_fractions = (0.4, 0.3, 0.3),
                     ),
                 ),
@@ -61,6 +101,7 @@ class TestEnsemble(unittest.TestCase):
                         species = self.ref_scenario.size.modes[0].species,
                         number = np.full(self.n, self.ref_scenario.size.modes[0].number),
                         geom_mean_diam = np.full(self.n, self.ref_scenario.size.modes[0].geom_mean_diam),
+                        log10_geom_std_dev = np.full(self.n, 1.6),
                         mass_fractions = (
                             np.full(self.n, self.ref_scenario.size.modes[0].mass_fractions[0]),
                             np.full(self.n, self.ref_scenario.size.modes[0].mass_fractions[1]),
@@ -109,6 +150,7 @@ class TestSampling(unittest.TestCase):
                         species = [so4, pom, soa, bc, dst, ncl],
                         number = scipy.stats.loguniform(3e7, 2e12),
                         geom_mean_diam = scipy.stats.loguniform(0.5e-7, 1.1e-7),
+                        log10_geom_std_dev = 1.6,
                         mass_fractions = [
                             scipy.stats.uniform(0, 1), # so4
                             scipy.stats.uniform(0, 1), # pom
@@ -123,6 +165,7 @@ class TestSampling(unittest.TestCase):
                         species = [so4, soa, ncl],
                         number = scipy.stats.loguniform(3e7, 2e12),
                         geom_mean_diam = scipy.stats.loguniform(0.5e-8, 3e-8),
+                        log10_geom_std_dev = 1.6,
                         mass_fractions = [
                             scipy.stats.uniform(0, 1), # so4
                             scipy.stats.uniform(0, 1), # soa
@@ -134,6 +177,7 @@ class TestSampling(unittest.TestCase):
                         species = [dst, ncl, so4, bc, pom, soa],
                         number = scipy.stats.loguniform(3e7, 2e12),
                         geom_mean_diam = scipy.stats.loguniform(1e-6, 2e-6),
+                        log10_geom_std_dev = 1.8,
                         mass_fractions = [
                             scipy.stats.uniform(0, 1), # dst
                             scipy.stats.uniform(0, 1), # ncl
@@ -148,6 +192,7 @@ class TestSampling(unittest.TestCase):
                         species = [pom, bc],
                         number = scipy.stats.loguniform(3e7, 2e12),
                         geom_mean_diam = scipy.stats.loguniform(1e-8, 6e-8),
+                        log10_geom_std_dev = 1.8,
                         mass_fractions = [
                             scipy.stats.uniform(0, 1), # pom
                             scipy.stats.uniform(0, 1), # bc
