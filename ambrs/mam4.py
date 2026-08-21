7ef7a2cc100053f62799708d78b1281f8df7cfbf
GlazerMann <kurt.glaesemann@pnnl.gov>
2026-08-20 21:36:15 -0500

    Fix typo in gas species name from 'soag' to 'SOAG'

--- ambrs/mam4.py
+++ ambrs/mam4.py
@@ -167,7 +167,7 @@ class GasMixingRatios:
         ih2so4 = GasSpecies.find(scenario.gases, 'H2SO4')
         if ih2so4 == -1:
             raise ValueError("H2SO4 gas not found in gas species")
-        isoag = GasSpecies.find(scenario.gases, 'soag')
+        isoag = GasSpecies.find(scenario.gases, 'SOAG')
         # fixme: double-check MAM units
         self.SO2 = scenario.gas_concs[iso2] * scenario.gases[iso2].molar_mass / dry_air_molar_mass
         self.H2SO4 = scenario.gas_concs[ih2so4] * scenario.gases[ih2so4].molar_mass / dry_air_molar_mass
